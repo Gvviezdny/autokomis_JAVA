0ba5f15744941b622d9d688e298d696583f37e83
Marcin <marcin.barsow@gmail.com>
2022-02-12 14:54:34 +0100

    Public class Player, money, cars, clients

---
+++ src/com/company/model/Player.java
@@ -0,0 +1,13 @@
+package com.company.model;
+
+import com.company.model.client.Client;
+import com.company.model.vehicle.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    public static int money;
+    public static List<Car> cars = new ArrayList<>();
+    public static List<Client> clients;
+}
