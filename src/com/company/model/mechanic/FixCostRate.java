597a8c2c87a08b4a74ac133ece4435871b400917
Marcin <marcin.barsow@gmail.com>
2022-02-12 11:23:05 +0100

    poprawki w mnożniku

--- src/com/company/model/mechanic/FixCostRate.java
+++ src/com/company/model/mechanic/FixCostRate.java
@@ -4,4 +4,26 @@ import com.company.model.vehicle.Brand;
 import com.company.model.vehicle.PartName;
 
 public class FixCostRate {
-}
+    private final Brand brand;
+    private final PartName partName;
+    private final int cost;
+
+    public FixCostRate(Brand brand, PartName partName, int cost) {
+        this.brand = brand;
+        this.partName = partName;
+        this.cost = cost;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public PartName getPartName() {
+        return partName;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+
